fd2938402cafc364d10f1af92e90d434a06a0ea8
Kubilay Ege <kubilayege@flamingo.gs>
2024-01-18 18:04:33 +0300

    Refactor NotificationService.cs
    Notification service variables exposed to config file to support standalone build sharing.

--- MHRS-OtomatikRandevu/Services/NotificationService.cs
+++ MHRS-OtomatikRandevu/Services/NotificationService.cs
@@ -1,18 +1,36 @@
 ﻿using MHRS_OtomatikRandevu.Services.Abstracts;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
+using System.Configuration;
+using System.Collections.Specialized;
+using MHRS_OtomatikRandevu.Utils;
 
 namespace MHRS_OtomatikRandevu.Services
 {
     public class NotificationService : INotificationService
     {
-        public const string TWILIO_ACCOUNT_SID = "TWILIO_ACCOUNT_SID";
-        public const string TWILIO_AUTH_TOKEN = "TWILIO_AUTH_TOKEN";
-        public const string TWILIO_PHONE_NUMBER = "TWILIO_PHONE_NUMBER";
-        public const string PHONE_NUMBER = "PHONE_NUMBER"; //Format -> +90XXXyyyZZww
+        public string? TWILIO_ACCOUNT_SID = "";
+        public string? TWILIO_AUTH_TOKEN = "";
+        public string? TWILIO_PHONE_NUMBER = "";
+        public string? PHONE_NUMBER = "";
 
         public NotificationService()
         {
+            if(string.IsNullOrEmpty(TWILIO_ACCOUNT_SID) || string.IsNullOrEmpty(TWILIO_AUTH_TOKEN) || string.IsNullOrEmpty(TWILIO_PHONE_NUMBER) || string.IsNullOrEmpty(PHONE_NUMBER))
+            {
+                TWILIO_ACCOUNT_SID = ConfigurationManager.AppSettings.Get(nameof(TWILIO_ACCOUNT_SID));
+                TWILIO_AUTH_TOKEN = ConfigurationManager.AppSettings.Get(nameof(TWILIO_AUTH_TOKEN));
+                TWILIO_PHONE_NUMBER = ConfigurationManager.AppSettings.Get(nameof(TWILIO_PHONE_NUMBER));
+                PHONE_NUMBER = ConfigurationManager.AppSettings.Get(nameof(PHONE_NUMBER));
+            }
+
+            if (string.IsNullOrEmpty(TWILIO_ACCOUNT_SID) || string.IsNullOrEmpty(TWILIO_AUTH_TOKEN) ||
+                string.IsNullOrEmpty(TWILIO_PHONE_NUMBER) || string.IsNullOrEmpty(PHONE_NUMBER))
+            {
+                ConsoleUtil.WriteText("Lütfen config dosyasındaki değerleri doldurun ve tekrar başlatın.", 2000);
+                return;
+            }
+            
             TwilioClient.Init(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
         }
 
